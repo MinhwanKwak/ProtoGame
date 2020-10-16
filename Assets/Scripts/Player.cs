8d65c9e3870c81148957b5be725cb28be1c6254a
moonjyo <m_jyo@naver.com>
2020-10-16 22:34:56 +0900

    test1
    test1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,21 +5,17 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-<<<<<<< HEAD
+
     public int abc;
+    public int def;
 
     // Start is called before the first frame update
     void Start()
     {
         abc = 0;
-=======
-    private bool isTest = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
->>>>>>> 362cc5ef3ae6373781e027d2ebcae1b1ae439c82
     }
+    private bool isTest = false;
+
 
     // Update is called once per frame
     void Update()
