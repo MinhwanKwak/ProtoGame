1b474d2a76cca3c03dd3f4c94515be15c78160bb
moonjyo <m_jyo@naver.com>
2020-11-11 17:29:51 +0900

    1111

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public CameraManager cameraManager;
+    public PlayerControll playercontroller;
+    public InputController inputController;
+
+    public static GameManager Instance;
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+
+        // DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
