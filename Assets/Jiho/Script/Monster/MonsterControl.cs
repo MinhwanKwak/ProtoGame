e0685adbd0fa9bef34005ebf773f055c620a5ea6
moonjyo <m_jyo@naver.com>
2020-12-09 00:00:43 +0900

    Monster hpui screenpoint fix

--- Assets/Jiho/Script/Monster/MonsterControl.cs
+++ Assets/Jiho/Script/Monster/MonsterControl.cs
@@ -67,11 +67,16 @@ public class MonsterControl : MonsterBasic
         base.Update();
 
         //uiHpBar.image.rectTransform.anchoredPosition = GameManager.Instance.cameraManager.GetMainCamera().WorldToScreenPoint(HpTransform.position);
+        uiHpBarArray[0].image.rectTransform.anchoredPosition = GameManager.Instance.cameraManager.GetMainCamera().WorldToScreenPoint(HpTransform.position);
+        uiHpBarArray[1].image.rectTransform.anchoredPosition = uiHpBarArray[0].image.rectTransform.anchoredPosition + new Vector2(40,0);
+        uiHpBarArray[2].image.rectTransform.anchoredPosition = uiHpBarArray[0].image.rectTransform.anchoredPosition - new Vector2(40,0);
+        //uiHpBarArray[1].image.rectTransform.anchoredPosition = GameManager.Instance.cameraManager.GetMainCamera().WorldToScreenPoint(HpTransform.position + Vector3.right);
+        //uiHpBarArray[2].image.rectTransform.anchoredPosition = GameManager.Instance.cameraManager.GetMainCamera().WorldToScreenPoint(HpTransform.position - Vector3.right);
 
-        for (int i = 0; i < uiHpBargoArray.Length; i++)
-        {
-            uiHpBarArray[i].image.rectTransform.anchoredPosition = GameManager.Instance.cameraManager.GetMainCamera().WorldToScreenPoint(HpTransformArray[i].position);
-        }
+        //for (int i = 0; i < uiHpBargoArray.Length; i++)
+        //{
+        //    uiHpBarArray[i].image.rectTransform.anchoredPosition = GameManager.Instance.cameraManager.GetMainCamera().WorldToScreenPoint(HpTransformArray[i].position);
+        //}
 
         if (monsterStatus == MonsterStatus.DEAD)
         {
